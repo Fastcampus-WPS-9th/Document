f992e8750b6d9a1de49dd597c2cb5b6b177f3b99
LeeHanYeong <dev@lhy.kr>
2018-10-11 17:03:07 +0900

    ABC의 관계필드에서 related_name에 %(class)s 를 사용해서 동적으로 값 지정

--- app/abstract_base_classes/models/related.py
+++ app/abstract_base_classes/models/related.py
@@ -21,7 +21,8 @@ class PostBase(models.Model):
         on_delete=models.CASCADE,
         # 유저(Person) 입장에서
         #  자신이 특정 Post의 'author'인 경우에 해당하는 모든 PostBase객체를 참조하는 역방향 매니저 이름
-        related_name='posts',
+        related_name='%(class)s_set',
+        related_query_name='%(class)s',
     )
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -30,6 +31,8 @@ class PostBase(models.Model):
 
 
 class PhotoPost(PostBase):
+    # author의 related_name
+    #  photopost_set
     photo_url = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
@@ -37,6 +40,8 @@ class PhotoPost(PostBase):
 
 
 class TextPost(PostBase):
+    # author의 related_name
+    #  textpost_set
     text = models.TextField(blank=True)
 
     def __str__(self):
